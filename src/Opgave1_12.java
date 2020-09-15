f6a822e17bab8ab0d44729bca3a80c2c33a2b0d2
jenna-P <jenn2464@edu.easj.dk>
2020-09-15 10:53:21 +0200

    Initial commit

---
+++ src/Opgave1_12.java
@@ -0,0 +1,15 @@
+public class Opgave1_12 {
+    public static void main(String[] args) {
+        System.out.println("24 * 1.6 = ");
+        System.out.println(24 * 1.6);
+        System.out.println("(60 * 60 + 40 * 60 + 35) / (60 * 60)");
+        System.out.println((60.0 * 60.0 + 40.0 * 60.0 + 35.0) / (60.0 * 60.0));
+        System.out.println("38.4 / 1.67");
+        System.out.print(38.4 / 1.67);
+        System.out.println("km/h");
+
+
+
+
+    }
+}
---
+++ src/Opgave1_5.java
@@ -0,0 +1,7 @@
+public class Opgave1_5 {
+    public static void main(String[] agrs) {
+        System.out.println("(7.5 * 6.5 - 4.5 * 3) / (47.5 - 5.5) = ");
+        System.out.println((7.5 * 6.5 - 4.5 * 3) / (47.5 - 5.5));
+    }
+}
+
